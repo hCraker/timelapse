def3a20d0fe250e1b32277532aa0bda4d4fd25d3
h_craker <heatherrosecraker@gmail.com>
2023-06-09 21:41:19 -0400

    Add LEDs, starttime, endtime, warnings

--- timelapse.py
+++ timelapse.py
@@ -1,37 +1,16 @@
 import cv2
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 import os
 from PIL import Image
 import numpy as np
 import psutil
 import threading
+import warnings
 
-#''' Process Monitoring
-class MemoryMonitorThread(threading.Thread):
-    def __init__(self):
-        super().__init__()
-        self._stop_event = threading.Event()
+import breadboard
 
-    def stop(self):
-        self._stop_event.set()
-
-    def run(self):
-        while not self._stop_event.is_set():
-            # Retrieve memory usage information
-            memory_info = psutil.virtual_memory()
-
-            # Print memory usage statistics
-            print(f"Total: {memory_info.total} bytes")
-            print(f"Available: {memory_info.available} bytes")
-            print(f"Used: {memory_info.used} bytes")
-            print(f"Percentage: {memory_info.percent}%\n")
-
-            # Sleep for a specific interval (e.g., 1 second)
-            time.sleep(1)
-#'''
-
-images_dir = "C:/Users/heath/OneDrive/Personal Projects/Timelapse/images"
+images_dir = "/home/heathercraker/Documents/timelapse/images"
 
 def create_images_dir(dir=None):
     global images_dir
@@ -86,46 +65,60 @@ def adjust_contrast(image, value):
     return adjusted_image
 
 def capture_image():
-    # Access the webcam
-    cap = cv2.VideoCapture(0)
+    '''
+    Function to capture an image every 30 seconds or more.
+    This minimum time allows the camera to warm up and get a quality image.
+    '''
+    try:
+        # Access the webcam
+        cap = cv2.VideoCapture(0)
 
-    # Check if the webcam is opened successfully
-    if not cap.isOpened():
-        print("Failed to open the webcam")
-        return
+        # Check if the webcam is opened successfully
+        if not cap.isOpened():
+            breadboard.errorLED()
+            raise Exceptbreadboardn("Failed to open the webcam")
+            return
 
-    # Read an image from the webcam
-    for i in range(0,30):
-        ret, frame = cap.read()
+        # Read an image from the webcam
+        for i in range(0,30):
+            ret, frame = cap.read()
 
-    # Check if the image was successfully captured
-    if not ret:
-        print("Failed to capture the image")
-        return
+        # Check if the image was successfully captured
+        if not ret:
+            now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            warnings.warn(f"Failed to capture the image: {now}")
+            breadboard.warningLED()
+            return
 
-    # Adjust the brightness and contrast of the image
-    adjusted_frame = frame
-    #adjusted_frame = adjust_brightness(frame, 100)  # Adjust brightness to make the image darker
-    #adjusted_frame = adjust_contrast(adjusted_frame, 1.5)  # Adjust contrast
- 
-    # Generate a human-readable filename with colons replaced by dashes
-    current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    filename = f"{images_dir}/image_{current_time}.png"
+        # Adjust the brightness and contrast of the image
+        adjusted_frame = frame
+        #adjusted_frame = adjust_brightness(frame, 100)  # Adjust brightness to make the image darker
+        #adjusted_frame = adjust_contrast(adjusted_frame, 1.5)  # Adjust contrast
+     
+        # Generate a human-readable filename with colons replaced by dashes
+        current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        filename = f"{images_dir}/image_{current_time}.jpg"
 
-    # Save the adjusted image
-    cv2.imwrite(filename, adjusted_frame)
+        # Save the adjusted image
+        cv2.imwrite(filename, adjusted_frame)
+        
+        # Release the webcam
+        cap.release()
 
-    # Release the webcam
-    cap.release()
+        print(f"Image captured: {filename}")
+        breadboard.successLED()
+    finally:
+        if cap.isOpened():
+            cap.release()
 
-    print(f"Image captured: {filename}")
+
 
 def create_gif(output_file):
     images = []
     
     # Get the list of JPEG files in the image directory
     for filename in os.listdir(images_dir):
-        if filename.endswith(".png"):
+        if filename.endswith(".jpg"):
             file_path = os.path.join(images_dir, filename)
             
             # Open each image and append it to the list
@@ -136,43 +129,37 @@ def create_gif(output_file):
     images[0].save(output_file, save_all=True, append_images=images[1:], loop=0, duration=200)
 
     print(f"GIF file created: {output_file}")
+    breadboard.successLED(3)
 
 def main():
+    breadboard.setupIO()
+
     create_images_dir()
 
-    monitor_thread = None
-    start_time = datetime.now()#(2023, 6, 7, 4, 0, 0) # time to start recording images
-    end_time = datetime(2023,6,7,18,0,0)
+    start_time = datetime.now() # time to start recording images
+    end_time = start_time + timedelta(days=1) # time to automatically stop recording
+    interval = timedelta(minutes=0) # time between images
+
     try:
-        # Start monitoring memory usage in a separate thread
-        #monitor_thread = MemoryMonitorThread()
-        #monitor_thread.start()
         print("Begin Image Capture")
-        while datetime.now() < end_time:
-            if datetime.now() > start_time:
+        previous = datetime.now()
+        capture_image()
+        while (datetime.now() < end_time) and (not breadboard.buttonStatus()):
+            now = datetime.now()
+            if (now > start_time) and (now - previous > interval):
+                previous = datetime.now()
                 capture_image()
-            time.sleep(60*5) # Wait
 
         print('Creating timelapse...')
-        start_str = start_time.strftime("%Y-%m-%d")
-        end_str = end_time.strftime("%Y-%m-%d")
-        gif_filename = f"./timelapse_{start_str}_{end_str}.gif"
-        create_gif(gif_filename)
-
-    except KeyboardInterrupt:
-        print('Keyboard Interrupt!')
-        
-        print('Creating timelapse...')
+        breadboard.allBlink(0.25, 2)
         start_str = start_time.strftime("%Y-%m-%d_%H-%M")
-        end_str = datetime.now().strftime("%Y-%m-%d_%H-%M")
-        gif_filename = f"./timelapse_{start_str}_{end_str}.gif"
+        end_str = end_time.strftime("%Y-%m-%d_%H-%M")
+        gif_filename = f"{images_dir}/timelapse_{start_str}_{end_str}.gif"
         create_gif(gif_filename)
-        
-        print("Cleaning up...")
-        #if monitor_thread is not None:
-        #            monitor_thread.stop()  # Signal the monitoring thread to stop
-        #            monitor_thread.join()  # Wait for the monitoring thread to finish
+       
         print("Exiting program.")
+    finally:
+        breadboard.cleanup()
         
 if __name__ == '__main__':
     main()
